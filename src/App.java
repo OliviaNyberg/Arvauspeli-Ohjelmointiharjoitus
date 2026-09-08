01e3da68acc687a7857790296d0ac6c30f5a63c1
Olivia Nyberg <amk1020386@student.hamk.fi>
2026-09-08 13:56:31 +0300

    Lisäsin String arvot ja tulostin testauksen

--- src/App.java
+++ src/App.java
@@ -3,6 +3,9 @@ public class App {
     public static void main(String[] args) throws Exception {
        Scanner in = new Scanner(System.in);
        String oikeaNimi = "Olivia";
-       
+       String arvaus = "";
+
+        System.out.println("Arvaa nimi!");
+
     }
 }
